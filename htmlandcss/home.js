b9f5cbacbc84db457953713e8cd50ffb437b922d
Melat woldegiorgis <123128037+Melatwolde@users.noreply.github.com>
2024-05-22 05:28:35 -0700

    Update home.js

--- htmlandcss/home.js
+++ htmlandcss/home.js
@@ -7,20 +7,13 @@ const searchResults = document.querySelector('.search-results');
 searchButton.addEventListener('click', () => {
   // Get the search query from the input field
   const query = searchInput.value;
-
-  // Make sure the query is not empty
   if (query) {
-    // Replace 'YOUR_API_KEY' with your actual API key
     const apiKey = '6797551cf07dc3e1736ab1eca6ab1029';
-
-    // Make a request to the Last.fm API
     fetch(`https://ws.audioscrobbler.com/2.0/?method=track.search&track=${query}&api_key=${apiKey}&format=json`)
     .then(response => response.json())
     .then(data => {
-      // Clear the results list
       resultsList.innerHTML = '';
 
-      // Add each track to the results list
       data.results.trackmatches.track.forEach(track => {
         const listItem = document.createElement('li');
         listItem.textContent = track.name;
